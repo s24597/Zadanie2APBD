b035b1b3dbc90088f60a045ab4a789ce24b033e9
Mikołaj Bembas <s24597@pjwstk.edu.pl>
2024-03-24 15:08:48 +0100

    Initial commit

---
+++ LegacyApp/Validation.cs
@@ -0,0 +1,15 @@
+﻿namespace LegacyApp;
+
+public class Validation
+{
+    public static bool Check_Name_And_Surname(string firstName, string lastName)
+    {
+        return string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName);
+    }
+    
+    public static bool Check_Mail(string email)
+    {
+        return !email.Contains("@") && !email.Contains(".");
+    }
+    
+}
